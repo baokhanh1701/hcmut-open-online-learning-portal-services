57b4b5a82bc147818350c7f37fc90c5a6804c900
baokhanh1701 <khanhnkb@gmail.com>
2023-11-28 00:52:28 +0700

    feature: add check login, not use yet

---
+++ src/middlewares/loginValidation.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+export const checkLogin = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized due to non-exist token" });
+    }
+    try {
+        const checkToken = token.split(" ")[1];
+        console.log(checkToken)
+        const decoded = jwt.verify(checkToken, process.env.JWT_SECRET_KEY);
+        req.userId = decoded.id;
+        next();
+    } catch (error) {
+        console.log(error.message)
+        return res.status(401).json({ message: "Unauthorized due to wrong token" });
+    }
+};
